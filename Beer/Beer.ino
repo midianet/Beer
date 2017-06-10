fe1b7815c0cd7cf31fca12aaf05326df3d87be4e
marcosfernandocosta <P@$w0rd>
2017-06-09 22:44:54 -0300

    Initial Commit

---
+++ Beer/Beer.ino
@@ -0,0 +1,64 @@
+//
+// Beer
+//
+// Description of the project
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Marcos Fernando
+// 				Marcos Fernando
+//
+// Date			09/06/17 22:44
+// Version		<#version#>
+//
+// Copyright	© Marcos Fernando, 2017
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
---
+++ Beer/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  Beer
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project Beer
+  Created by Marcos Fernando on 09/06/17
+  Copyright © 2017 Marcos Fernando
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2017
+  All rights reserved
+  http://embedXcode.weebly.com
+
